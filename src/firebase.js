da8de6d8f42b88a4465e613d9dfddf6f427fc49c
Loubna Sellam <loubnasellam49@gmail.com>
2025-06-02 18:05:58 +0200

    git commit

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,14 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCS1eYun6cDYO_6L4fNRWiBL6t5E6l2Ls',
+  apiKey: 'AIzaSyCS1eYun6cDYO_6LAfdRNWiBL6t5E6l2Ls',
   authDomain: 'famnas-c89ce.firebaseapp.com',
   projectId: 'famnas-c89ce',
-  storageBucket: 'famnas-c89ce.appspot.com',
+  storageBucket: 'famnas-c89ce.firebasestorage.app',
   messagingSenderId: '750744958470',
   appId: '1:750744958470:web:e96cddfd7ee2873bb93587',
   measurementId: 'G-XEG2B4E46Q',
 };
-
 // ✅ Initialiser l'app Firebase
 const app = initializeApp(firebaseConfig);
 
